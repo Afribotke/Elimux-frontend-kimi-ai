352c78c04901e8094ff81ec9863388cdcb80531e
Afribotke <267673311+Afribotke@users.noreply.github.com>
2026-06-28 04:59:58 +0300

    fix: sitemap quotes and AI router regex

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -20,10 +20,10 @@ export async function GET() {
     urlEntries += "  </url>" + "\n";
   }
 
-  const sitemap = "<?xml version=""1.0"" encoding=""UTF-8""?>" + "\n" +
-    "<urlset xmlns=""http://www.sitemaps.org/schemas/sitemap/0.9"">" + "\n" +
+  const sitemap = '<?xml version="1.0" encoding="UTF-8"?>' + "\n" +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + "\n" +
     urlEntries +
-    "</urlset>";
+    '</urlset>';
 
   return new NextResponse(sitemap, {
     headers: {
